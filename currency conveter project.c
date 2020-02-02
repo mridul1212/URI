b9a3dfb0061f1a59a01a675d52b6ff2af20e5edb
Mridul <54906968+mridul1212@users.noreply.github.com>
2020-02-02 22:45:13 +0600

    Add files via upload

---
+++ currency conveter project.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main ()
+{
+    long long int j,i=123456789101112;
+    if (i%3==0)
+        printf("yes");
+
+    j=i/3;
+            printf("div is %lld\n",j);
+            printf("%lld",j*3);
+
+}
