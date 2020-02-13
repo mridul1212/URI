26613eda3ca3e49fac5c73f4e7d3cad9d6501f4d
ArjoArjun <54906968+mridul1212@users.noreply.github.com>
2020-02-14 03:40:30 +0600

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,16 +1,17 @@
 #include <iostream>
-#include<stdio.h>
-#include<math.h>
 
 using namespace std;
 
 int main()
 {
-    double a,b,c,d;
+    double a, n=0.0;
     cin>>a;
-    b= (1+sqrt(5))/2.0;
-    c = (1-sqrt(5))/2.0;
-    d= (pow(b,a))-(pow(c,a))/sqrt(5);
-    printf("%.1lf\n",d);
+    while(a--)
+    {
+        n+=2.0;
+        n=1.0/n;
+    }
+    n+=1.0;
+    printf("%.10lf\n",n);
     return 0;
 }
