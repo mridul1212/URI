383c4f8f283904334e018b9f4fc4cbdeb52f821d
ArjoArjun <54906968+mridul1212@users.noreply.github.com>
2020-02-14 12:29:30 +0600

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -4,14 +4,13 @@ using namespace std;
 
 int main()
 {
-    double a, n=0.0;
-    cin>>a;
-    while(a--)
+    int unsigned long a,b;
+    while(cin>>a>>b)
     {
-        n+=2.0;
-        n=1.0/n;
+        if(a==0&&b==0)
+            break;
+        else
+            cout<<a*b<<endl;
     }
-    n+=1.0;
-    printf("%.10lf\n",n);
     return 0;
 }
